10d0e15eadef68729127863a797b302a6a469acf
LKchemposer <lkchemposer@gmail.com>
2021-01-16 01:51:43 -0500

    add stats to nb, add in gitignore

---
+++ nb/cliffs_delta.py
@@ -0,0 +1,35 @@
+# adapted from https://github.com/neilernst/cliffsDelta
+def cliffs_delta(l1, l2):
+    m, n = len(l1), len(l2)
+    l2 = sorted(l2)
+    j = more = less = 0
+    
+    for repeats, x in gen(sorted(l1)):
+        while j <= (n - 1) and l2[j] < x:
+            j += 1
+        more += j * repeats
+        while j <= (n - 1) and l2[j] == x:
+            j += 1
+        less += (n - j) * repeats
+    d = (more - less) / (m * n)
+    
+    # Hess and Kromrey, 2004
+    thresholds = {
+        'small': 0.147,
+        'medium': 0.33,
+        'large': 0.474
+    }
+    size = 'negligible' if d < thresholds['small'] else 'small' if d < thresholds['medium'] else 'medium' if d < thresholds['large'] else 'large'
+    
+    return d, size
+
+def gen(lst):
+    '''Iterator, chunks repeated values.'''
+    for j, two in enumerate(lst):
+        if j == 0:
+            one, i = two, 0
+        if one != two:
+            yield j - i, one
+            i = j
+        one = two
+    yield j - i + 1, two
